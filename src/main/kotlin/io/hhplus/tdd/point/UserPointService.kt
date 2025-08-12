ded564569bcd8b20875f13bfc0abd46776cdb12e
Hayden <dlsrb0412@naver.com>
2025-08-13 01:34:49 +0900

    feat: 🟢유저포인트 조회 기능을 구현

--- src/main/kotlin/io/hhplus/tdd/point/UserPointService.kt
+++ src/main/kotlin/io/hhplus/tdd/point/UserPointService.kt
@@ -6,7 +6,6 @@ import org.springframework.stereotype.Service
 @Service
 class UserPointService(private val userPointTable: UserPointTable) {
     fun getUserPoint(userId: Long): UserPoint {
-        // TODO: 특정 유저의 포인트를 조회하는 기능을 작성해주세요.
-        return UserPoint(0, 0, 0)
+        return userPointTable.selectById(userId)
     }
 }
